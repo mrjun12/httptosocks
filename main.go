474435082195ce805bb2a279c679d8b720279996
MrJun <admin@lofter.cc>
2020-07-16 11:39:18 +0800

    第一版

--- main.go
+++ main.go
@@ -37,46 +37,36 @@ func main() {
 	}
 	client = newClient(socks5proxy)
 	hndl := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		if req.Method == "CONNECT" {
-			serverConn, err := socks5proxy.Dial("tcp", req.Host)
-			if err != nil {
-				w.WriteHeader(500)
-				w.Write([]byte(err.Error() + "\n"))
-				return
-			}
-			hijacker, ok := w.(http.Hijacker)
-			if !ok {
-				serverConn.Close()
-				w.WriteHeader(500)
-				w.Write([]byte("Failed cast to Hijacker\n"))
-				return
-			}
-			w.WriteHeader(200)
-			_, bio, err := hijacker.Hijack()
-			if err != nil {
-				w.WriteHeader(500)
-				w.Write([]byte(err.Error() + "\n"))
-				serverConn.Close()
-				return
-			}
-			go io.Copy(serverConn, bio)
-			go io.Copy(bio, serverConn)
-		} else {
-			// Server-Only field; we get an error fi we pass this to `client.Do`.
-			req.RequestURI = ""
-
-			resp, err := client.Do(req)
-			if err != nil {
-				w.WriteHeader(500)
-				w.Write([]byte(err.Error() + "\n"))
-				return
-			}
-			hdr := w.Header()
-			for k, v := range resp.Header {
-				hdr[k] = v
-			}
-			w.WriteHeader(resp.StatusCode)
-			io.Copy(w, resp.Body)
+		if req.Method != "CONNECT" {
+			return
 		}
+
+		serverConn, err := socks5proxy.Dial("tcp", req.Host)
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte(err.Error() + "\n"))
+			return
+		}
+		hijacker, ok := w.(http.Hijacker)
+		if !ok {
+			serverConn.Close()
+			w.WriteHeader(500)
+			w.Write([]byte("Failed cast to Hijacker\n"))
+			return
+		}
+		w.WriteHeader(200)
+		_, bio, err := hijacker.Hijack()
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte(err.Error() + "\n"))
+			serverConn.Close()
+			return
+		}
+		go io.Copy(serverConn, bio)
+		go io.Copy(bio, serverConn)
+
 	})
+
+	http.ListenAndServe(*laddr, hndl)
+
 }
